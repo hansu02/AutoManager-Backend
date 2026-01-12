f9e484f73f793c4e9c1667e43083726c8cfb2e59
hansu02 <baris.hansu57@outlook.com>
2026-01-12 15:30:20 +0300

    chore: init project structure

---
+++ AutoManager-Backend/src/main/java/com/automanager/backend/AutoManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.automanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoManagerBackendApplication.class, args);
+    }
+
+}
---
+++ AutoManager-Backend/src/test/java/com/automanager/backend/AutoManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automanager.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
